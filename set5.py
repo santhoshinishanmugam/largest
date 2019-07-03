a3e0e418addcf94cc42c1a2a21a598e49b69e6b8
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-03 11:53:28 +0530

    Update set5.py

--- set5.py
+++ set5.py
@@ -1,9 +1,9 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if (a>b and a>c):
+if (a > b and a > c):
   print(a)
-elif (b>a and b>c):
+elif (b > a and b > c):
   print(b)
-else:
+elif (c > a and c > b):
   print(c)
